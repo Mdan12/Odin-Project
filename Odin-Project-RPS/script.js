06ff0162699eff7d9ee75960b830e4fcba54a8c9
mdan_12 <magnusde93@gmail.com>
2022-09-03 15:38:31 +0000

    RPS updated

--- Odin-Project-RPS/script.js
+++ Odin-Project-RPS/script.js
@@ -1,33 +1,55 @@
+let wins = 0
+let losses = 0
 
-
-const rps = ['rock', 'paper', 'scissors'];
-
-const randomIndex = rps[Math.floor(Math.random())];
-
-let userInput = prompt("Please type either rock, paper or scissors")
-
-function winnerLoser() {
-
-    if (randomIndex == "rock" && userInput == "scissors") {
-        console.log("computer wins"); 
-    } else if (randomIndex == "scissors" && userInput == "paper") {
-        console.log("computer wins"); 
-    } else if (randomIndex == "paper" && userInput == "rock") {
-        console.log("computer wins"); 
-    } else if (randomIndex == "rock" && userInput == "paper") {
-        console.log("user wins"); 
-    } else if (randomIndex == "paper" && userInput == "scissors") {
-        console.log("user wins"); 
-    } else if (randomIndex == "scissors" && userInput == "rock") {
-        console.log("user wins"); 
-    } else if (randomIndex === userInput) {
-        console.log("Draw")
-    } else {
-        console.log("Wrong pick")
+for (let i = 0; i < 5;) {
+    if (wins = 5) {
+        console.log("You win.");
+        i = 5;
+    } else if (losses = 5) {
+        console.log("You lost.");
+        i = 5;
     }
 
+
+    function getComputerChoice() {
+        
+        const rps = ['rock', 'paper', 'scissors'];
+        const computerRandom = rps[Math.floor(Math.random())];
+        return computerRandom
+    }
+
+    let computerChoice = getComputerChoice();
+
+    let playerSelection = prompt("Please type either rock, paper or scissors").toLowerCase();
+
+
+    function winnerLoser() {
+
+        if (computerChoice == "rock" && playerSelection == "scissors") {
+            console.log("computer wins"); 
+            losses++;
+        } else if (computerChoice == "scissors" && playerSelection == "paper") {
+            console.log("computer wins");
+            losses++;
+        } else if (computerChoice == "paper" && playerSelection == "rock") {
+            console.log("computer wins"); 
+            losses++;
+        } else if (computerChoice == "rock" && playerSelection == "paper") {
+            console.log("user wins");
+            wins++; 
+        } else if (computerChoice == "paper" && playerSelection == "scissors") {
+            console.log("user wins");
+            wins++;
+        } else if (computerChoice == "scissors" && playerSelection == "rock") {
+            console.log("user wins"); 
+            wins++
+        } else if (computerChoice === playerSelection) {
+            console.log("Draw")
+        } else {
+            console.log("Wrong pick")
+        }
+
+    }
+
+
 }
-
-
-
-console.log(winnerLoser())
